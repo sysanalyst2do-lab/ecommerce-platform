83f4b946f148004e88164bd97212ec5d0bb2d08c
Sodlet <Sodlet@mail.ru>
2026-04-06 23:38:53 +0300

    Добработка связей бека и фронта для работы в контейнере

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,7 +5,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      "/api": "http://localhost:8000",
+      "/api": process.env.API_URL || "http://localhost:8000",
     },
   },
 });
